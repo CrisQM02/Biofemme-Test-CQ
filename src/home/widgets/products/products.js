95615d51d88a1e90c9c518cccb13552ac99644da
CristinaQM <cequinte@espol.edu.ec>
2024-10-25 21:57:11 -0500

    Updates:
    - Transition animation fixed
    - Addition of Icons
    - New element in products list

--- src/home/widgets/products/products.js
+++ src/home/widgets/products/products.js
@@ -1,15 +1,17 @@
 import "./products.css"
+import Icon from "@mdi/react";
 
 import p1 from "./products_images/p1-lifting-serum.jpg";
 import p2 from "./products_images/p2-Sunscreen-50.jpg";
 import p3 from "./products_images/p3-BBOIL.jpg";
 import p4 from "./products_images/p4-Mini-Kit.jpg";
+import { mdiCart } from "@mdi/js";
 
 const products = [
-    {id: 1, name: "LIFTING SERUM", type: "Sérum Tensor", description: "Proage | Todo tipo de piel", price: 21.57, img: p1, url:"https://compras.biofemme.com.ec/producto/lifting-serum-serum-tensor/"},
-    {id: 2, name: "SUNSCREEN 50+", type: "Bloqueador Solar", description: "Protección | Todo tipo de piel", price: 24.64, img: p2, url:"https://compras.biofemme.com.ec/producto/sunscreen-50-bloqueador-solar-50/"},
-    {id: 3, name: "BIO BEAUTY OIL", type: "Aceite Seco Hidratante", description: "Hidratación | Todo tipo de piel", price: 16.42, img: p3, url:"https://compras.biofemme.com.ec/producto/bio-beauty-oil-aceite-seco-hidratante/"},
-    {id: 4, name: "BIOFEMME MINI KIT", type: "Mini Sizes", description: "Sets | Piel mixta a grasa", price: 15.00, img: p4, url:"https://compras.biofemme.com.ec/producto/biofemme-mini-kit/"},
+    {id: 1, name: "LIFTING SERUM", type: "Sérum Tensor", description: "Proage | Todo tipo de piel", price: 21.57, oldPrice: null, img: p1, url:"https://compras.biofemme.com.ec/producto/lifting-serum-serum-tensor/"},
+    {id: 2, name: "SUNSCREEN 50+", type: "Bloqueador Solar", description: "Protección | Todo tipo de piel", price: 24.64, oldPrice: null, img: p2, url:"https://compras.biofemme.com.ec/producto/sunscreen-50-bloqueador-solar-50/"},
+    {id: 3, name: "BIO BEAUTY OIL", type: "Aceite Seco Hidratante", description: "Hidratación | Todo tipo de piel", price: 16.42, oldPrice: null, img: p3, url:"https://compras.biofemme.com.ec/producto/bio-beauty-oil-aceite-seco-hidratante/"},
+    {id: 4, name: "BIOFEMME MINI KIT", type: "Mini Sizes", description: "Sets | Piel mixta a grasa", price: 15.00, oldPrice: 20.70, img: p4, url:"https://compras.biofemme.com.ec/producto/biofemme-mini-kit/"},
 ]
 
 const ProductsSection = () => {
@@ -27,9 +29,10 @@ const ProductsSection = () => {
                         <p className="title">{product.name}</p>
                         <p className="type">{product.type}</p>
                         <p className="description">{product.description}</p>
-                        <p className="price">{"$"+ product.price.toFixed(2)}</p>
-
+                        <p className="price">{product.oldPrice !== null && (<s>{"$" + product.oldPrice.toFixed(2) + " "}</s>)}{("$"+ product.price.toFixed(2))}</p>
                         <div className="car-label">
+                            <Icon className="cart-icon" path={mdiCart} size={0.8}/>
+                            <div className="my-space"></div>
                             <p>Añadir al carrito</p>
                         </div>
                     </div>
--- src/home/widgets/search_field/search_field.js
+++ src/home/widgets/search_field/search_field.js
@@ -1,11 +1,12 @@
-import "./search_field.css"
 import Icon from "@mdi/react";
+import { mdiMagnify} from "@mdi/js";
+import "./search_field.css"
 
 const MySearchField = () => {
     return (
         <div className="search-field">
             <input type="text" className="my-textfield" placeholder="¿Qué producto buscas?" />
-            <button className="search-button" type="button">AAA</button>
+            <button className="search-button" type="button"><Icon className="material-icons btn-icon" path={mdiMagnify} size={1}/></button>
         </div>
     )
 }
