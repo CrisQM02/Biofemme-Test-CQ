7448366b2159cddd2f93a63ab35de949be67193f
CristinaQM <cequinte@espol.edu.ec>
2024-10-26 22:07:55 -0500

    React Router Bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 
 import Home from "./home/page";
 import MyAppBar from './widgets/my_app_bar';
